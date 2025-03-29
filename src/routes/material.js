d2d0191dcb5aae50c13fdc1a185e8bd8ea9d4392
CodeXmike <simplymike07@gmail.com>
2025-03-29 10:43:16 +0100

    Third commit

--- src/routes/material.js
+++ src/routes/material.js
@@ -1,7 +1,7 @@
 // routes/material.js
 import express from 'express';
 import multer from 'multer';
-import { uploadMaterial, getAllMaterials, getMaterialsByCourse } from '../controllers/materialController.js';
+import { uploadMaterial, getAllMaterials, getMaterialsByCourse, downloadMaterial } from '../controllers/materialController.js';
 import { protect } from '../middleware/authMiddleware.js';
 import { storage } from '../config/cloudinary.js'; // ✅ Use cloudinary storage
 
@@ -12,7 +12,7 @@ const upload = multer({ storage }); // ✅ now uses Cloudinary
 router.post('/upload', protect, upload.single('file'), uploadMaterial);
 
 
-router.get('/download/:id', getAllMaterials);
+router.get('/download/:id', downloadMaterial);
 router.get('/', getAllMaterials);
 router.get('/course/:courseCode', getMaterialsByCourse);
 
